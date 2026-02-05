c5f57b6f45e85d21251da1f5ffa6127b170101ab
Weston Hopkins <whopkins@anamosa.k12.ia.us>
2026-02-05 17:54:21 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var counter = 100;
 
+var prefixIncrement =  ++counter
+
+var postfixIncrement = counter++
+
+var prefixDecrement = --counter
+
+var postfixDecrement = counter--
+
+document.getElementById("result").innerHTML = counter
